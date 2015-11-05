a28f402c47b9a52d11e3f4c46d603bf179c9d4d4
vivek <vhprasad23@yahoo.com>
2015-11-05 07:31:39 -0500

    Problems from Level 4 - Linked lists, stacks and queues

---
+++ src/com/LinkedLists/InterviewBit/ListNode.java
@@ -0,0 +1,13 @@
+package com.LinkedLists.InterviewBit;
+
+/**
+ * Created by priyavivek on 11/2/15.
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
